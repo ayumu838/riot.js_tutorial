a0098d66e8c034023b980638e760f66b12734276
ayumu.mochizuki <ayumu839.a@gmail.com>
2020-12-29 02:10:50 +0900

    add animation

--- capter5/src/main.js
+++ capter5/src/main.js
@@ -3,6 +3,5 @@ import App from './app.riot'
 import { component } from 'riot'
 
 component(App)(document.getElementById('app'), {
-  title: 'Hello Riot.js',
-  subTitle: 'This is parent'
+  title: 'Riot.js with Anime.js',
 })
